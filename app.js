edc33b7d41a6ad474401c64f0982f89e0298569b
Ian Benedict <ianbenedict@gmail.com>
2014-10-09 18:43:56 -0400

    Added basic webhook test.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 var express = require('express'),
+    settings = require('./settings.json'),
     router = express.Router(),
     app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-router.get('/', function (request, response) {
-  response.sendStatus(200);
+router.get('/', function (req, res) {
+  res.sendStatus(200);
 });
  
-router.post('/', function (request, response) {
-  console.log('Hit!');
+router.post('/', function (req, res) {
+  var options = {
+    url: settings['post_endpoint'],
+    body: '{ "text": "Vayne is the best ADC ever." }'
+  };
+
+  req.post(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log('Successful POST.');
+    }
+  });
 });
  
 app.use('/', router);
