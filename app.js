c100cbfd7319867d7706784d046a86e1c62ffc13
Ian Benedict <ianbenedict@gmail.com>
2014-10-10 18:44:42 -0400

    Removed old json field name.

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ router.post('/', function (req, res) {
     if (data.meta.code === 200) {
       options.body = create_slack_message(
         ['#', req.body.channel_name].join(''),
-        icon_url:["https://files.slack.com/files-pri/T02NRS0CK-F02P8KAPN/", data.result.champ_name, ".png"].join(''),
+        ["https://files.slack.com/files-pri/T02NRS0CK-F02P8KAPN/", data.result.champ_name, ".png"].join(''),
         [data.result.champ_name, 'says: '].join(''),
         [
           '@', req.body.user_name, ': ',
