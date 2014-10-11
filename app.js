7c33cfe6bfd4b562e845e624acbb0e2a5160cf56
John Tai <zapyre_1@hotmail.com>
2014-10-11 01:29:42 -0400

    changing up the message a bit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ router.post('/', function (req, res) {
     if (data.meta.code === 200) {
       options.body = create_slack_message(
         ['#', req.body.channel_name].join(''),
-        ["https://files.slack.com/files-pri/T02NRS0CK-F02P935NH/morgana.png"].join(''),
+        ["http://lorempixel.com/48/48/"].join(''),
         [data.result.champ_name, 'says: '].join(''),
         [
           '@', req.body.user_name, ': ',
