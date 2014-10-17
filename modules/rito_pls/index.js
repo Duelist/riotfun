22fd377108083443772b1a1dac1d38f263020b3a
John Tai <zapyre_1@hotmail.com>
2014-10-17 09:58:37 -0400

    Adding back error message when summoner's games not found

--- modules/rito_pls/index.js
+++ modules/rito_pls/index.js
@@ -204,9 +204,6 @@ module.exports = function () {
                 }
               });
             }
-            if (errorFlag === true){
-              return callback (create_response(null, 404, 'No recent games found between these summoners.'));
-            }
           } else {
             return callback(create_response(null, 404, 'Summoner(s) not found.'));
           }  
